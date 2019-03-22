23bcb04922c2de857192833ad4606c7b32108905
kishorSingh8233 <awadh.kishor@hashworks.co>
2019-03-22 08:50:16 +0530

    OAuth2 Implemented Successfully

--- src/main/java/com/hashworks/UserManagementService/model/User.java
+++ src/main/java/com/hashworks/UserManagementService/model/User.java
@@ -1,17 +1,82 @@
 package com.hashworks.UserManagementService.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.*;
 
+
+@Entity
 public class User {
 
-    private String userName;
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+//    @Column
+//    private String firstName;
+//    @Column
+//    private String lastName;
+    @Column
+    private String username;
+    @Column
+    @JsonIgnore
     private String password;
+    @Column
+//    private long salary;
+//    @Column
+//    private int age;
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public long getId() {
+        return id;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setId(long id) {
+        this.id = id;
     }
+//
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+
+    public String getUsername() {
+        return username;
+    }
+
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+
+    public String getPassword() {
+        return password;
+    }
+
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+
+//    public long getSalary() {
+//        return salary;
+//    }
+//
+//    public void setSalary(long salary) {
+//        this.salary = salary;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
 }
